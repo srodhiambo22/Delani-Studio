82c257892af19326cfe54b315d387d2c0c3e6be4
Samuel Odhiambo <samuel.odhiambo01@student.moringaschool.com>
2023-04-22 22:21:55 +0300

    js file update

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-  $(document).ready(function(){
-    $("form").submit(function(){
-      alert("Your message has been received");
-    });
+$(document).ready(function(){
+  $("form").submit(function(){
+    alert("Your message has been received");
   });
+});
   $(document).ready(function() {
-    $(".hover-effect").hover(function() {
-      $(this).toggleClass("hover-text");
+    $('.image-text-container').hover(function() {
+      $(this).find('img').hide();
+      $(this).find('.text').show();
+    }, function() {
+      $(this).find('.text').hide();
+      $(this).find('img').show();
     });
   });
